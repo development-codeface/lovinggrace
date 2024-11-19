b06856ce7c80162615071936e07e67056860f431
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-11-19 17:54:54 +0530

    Update clinet.ts

--- libs/contents/wordpress/clinet.ts
+++ libs/contents/wordpress/clinet.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://lga.ddhotel.in/graphql";
+const API_URL = "https://cfuat.in/lgadxb/graphql";
 const WORDPRESS_AUTH_REFRESH_TOKEN = "";
 
 export { API_URL, WORDPRESS_AUTH_REFRESH_TOKEN };
